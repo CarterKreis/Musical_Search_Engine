9f260b4e8e533db760b55bc37bd18decd3866394
CarterKreis <cckreis0507@gmail.com>
2023-11-28 17:09:30 -0500

    search_artist testing & commenting

--- src/MusicalSearchEngine.py
+++ src/MusicalSearchEngine.py
@@ -5,21 +5,22 @@ from API import Genius_API_Key
 
 def testGenius():
     # Examples of using lyricsgenius
+    # setting verbose to false gets rid of print() statements
     genius = lyricsgenius.Genius(Genius_API_Key.token)
+    genius.verbose = False
 
-    # artist = genius.search_artist("Andy Shauf", max_songs=2, sort="title", include_features=True)
-    # print(artist.songs)
+    # Includes songs where Andy is the main Artist for song
+    # Can sort by "title" or "popularity"
+    artist = genius.search_artist("Andy Shauf", max_songs=2, sort="title", include_features=True)
+    for song in artist.songs:
+        song.lyrics = ""
+        print(song)
 
     # genius.search_artist will always choose the same artist
     # e.g. search_artist("Luke") will always return Luke Combs
-    num = 5
-    for i in range(num):
-        artist = genius.search_artist("Luke", max_songs=2)
-        print(artist.songs)
 
 
 if __name__ == "__main__":
-    print("Welcome to the Musical Search Engine")
+    print("Welcome to the Musical Search Engine\n")
 
     testGenius()
-
