7569b14f00a2dc8301eaedd252099f234a2e2139
CarterKreis <cckreis0507@gmail.com>
2023-12-02 22:08:30 -0500

    added testSongs() to show how .search_songs() works

--- src/MusicalSearchEngine.py
+++ src/MusicalSearchEngine.py
@@ -4,7 +4,7 @@ from API import Genius_API_Key
 
 
 # Function to show some of lyricsgenius functionality
-def testGenius():
+def demoGenius():
     # Examples of using lyricsgenius
     # setting verbose to false gets rid of print() statements
     genius = lyricsgenius.Genius(Genius_API_Key.token)
@@ -21,7 +21,18 @@ def testGenius():
     # e.g. search_artist("Luke") will always return Luke Combs
 
 
-# Prints out Mainmenu
+# searches top 20 songs with the keyword "songs" and prints out the title of the songs
+def testSongs():
+    genius = lyricsgenius.Genius(Genius_API_Key.token)
+    genius.verbose = False
+
+    songs = genius.search_songs("girl", per_page=50)
+
+    for song in songs['hits']:
+        print("Song:", song['result']['full_title'], "\nArtist:", song['result']['artist_names'], "\n")
+
+
+# Prints out Main-menu
 def printMenu():
     print("What action would you like to perform?")
     print("Please input the number for each option")
@@ -46,6 +57,8 @@ if __name__ == "__main__":
     print("Welcome to the Musical Search Engine")
     print("------------------------------------")
 
+    testSongs()
+
     menu = True
     while menu:
         printMenu()
@@ -56,15 +69,21 @@ if __name__ == "__main__":
             subMenu("Sort")
             subChoice = input()
 
+
+
         # Searching
         elif mainChoice == "2":
             subMenu("Search")
             subChoice = input()
 
+
+
         # Comparing
         elif mainChoice == "3":
             subMenu("Compare")
             subChoice = input()
+
+
 
         # Exit option
         elif mainChoice == "4":
@@ -74,4 +93,4 @@ if __name__ == "__main__":
         else:
             print("Please type a number 1-4")
 
-    # testGenius()
+    # demoGenius()
