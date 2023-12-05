b406191f3368f0fd19ad8137c910fc85fdd774e8
Carter Kreis <116681287+CarterKreis@users.noreply.github.com>
2023-12-04 23:55:05 -0500

    Deleted

--- src/AdjList.py
+++ src/AdjList.py
@@ -1,19 +1,1 @@
-from Map import *
 
-
-class AdjList:
-
-    def __init__(self):
-        self.graph = Map()
-
-    def insert(self, key, value):
-        if key not in self.graph:
-            self.graph.insert(key, value=[])
-
-        self.graph.insert(key, value.append(value))
-
-    def search(self, key):
-        if key in self.graph:
-            return True
-        else:
-            return False
